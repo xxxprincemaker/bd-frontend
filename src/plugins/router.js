2ec84f83eb5694f4fcde06e422a31426595b1f49
YagoDelfino <daventhidas@gmail.com>
2022-03-07 22:30:33 -0300

    Tudo pronto, menos os gráficos

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -10,13 +10,13 @@ const routes = [
     component: () => import("../views/HomeView.vue"),
   },
   {
-    path: "/about",
-    name: "about",
+    path: "/movies",
+    name: "filmes",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "about" */ "../views/FilmView.vue"),
   },
 ];
 
