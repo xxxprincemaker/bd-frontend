cef818030d67abe8eb4bf1f92e0061bfb8da33d8
Prince <luizfas@dcc.ufrj.br>
2022-03-08 18:57:39 -0300

    Melhorando FilmView.vue

--- src/views/FilmView.vue
+++ src/views/FilmView.vue
@@ -1,36 +1,43 @@
 <template>
   <div class="filmes">
      <v-card-title class="justify-left; pl-9" style="color: whitesmoke;"> FILMES COM 3 HORAS OU MAIS DE DURAÇÃO </v-card-title>
-     <Grafico></Grafico>
+     <Grafico :optionss="this.rotaQuery1"></Grafico>
      <br>
-    <v-card-title class="justify-left; pl-9" style="color: whitesmoke;"> FILMES COM 3 HORAS OU MAIS DE DURAÇÃO </v-card-title>
-    <Grafico></Grafico>
-         <br>
-    <v-card-title class="justify-left; pl-9" style="color: whitesmoke;"> FILMES COM 3 HORAS OU MAIS DE DURAÇÃO </v-card-title>
-    <Grafico></Grafico>
-         <br>
-    <v-card-title class="justify-left; pl-9" style="color: whitesmoke;"> FILMES COM 3 HORAS OU MAIS DE DURAÇÃO </v-card-title>
-    <Grafico></Grafico>
-         <br>
-    <v-card-title class="justify-left; pl-9" style="color: whitesmoke;"> FILMES COM 3 HORAS OU MAIS DE DURAÇÃO </v-card-title>
-    <Grafico></Grafico>
-         <br>
-    <v-card-title class="justify-left; pl-9" style="color: whitesmoke;"> FILMES COM 3 HORAS OU MAIS DE DURAÇÃO </v-card-title>
-    <Grafico></Grafico>
-         <br>
-    <v-card-title class="justify-left; pl-9" style="color: whitesmoke;"> FILMES COM 3 HORAS OU MAIS DE DURAÇÃO </v-card-title>
-    <Grafico></Grafico>
-         <br>
   </div>
 </template>
 <script>
 
 import Grafico from "@/components/Grafico";
+import rotasService from "@/services/rotas.service";
 
 export default({
 components: {
     Grafico
   },
+  data: () => {
+    return {
+      rotaQuery1: Object,
+      rotaQuery2: Object,
+      rotaQuery3: Object,
+      rotaQuery4: Object,
+      rotaQuery5: Object,
+      rotaQuery6: Object,
+      rotaQuery7: Object,
+      rotaQuery8: Object,
+      rotaQuery9: Object,
+      rotaQuery10: Object,
+    };
+  },
+  methods: {
+    async importarQuery(rota){
+      await rotasService.recuperarRota(rota).then(response => {
+        this.rotaQuery1 = response.data;
+      }).catch( () => {} )
+    }
+  },
+  mounted() {
+    this.importarQuery(1);
+  }
 })
 </script>
 
